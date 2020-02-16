ca33fd9ca092cf4eaa15f196b20262b85df75b7b
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2020-02-16 18:45:38 +0900

    入力バッファをシステムのメンバ変数からリソースに移動

--- src/system.rs
+++ src/system.rs
@@ -1,28 +1,21 @@
 use crate::traits::InputParser;
 use amethyst::{
-    ecs::{Read, System},
+    ecs::{Read, System, SystemData, World, Write, WriteExpect},
     input::InputHandler,
+    shrev::EventChannel,
     utils::circular_buffer::CircularBuffer,
 };
 
 use std::marker::PhantomData;
 
-pub struct InputHandleSystem<I>
-where
-    I: InputParser,
-{
+pub struct InputHandleSystem<I> {
     _marker: PhantomData<I>,
-    input_buffer: CircularBuffer<I::InputSignal>,
 }
 
-impl<I> InputHandleSystem<I>
-where
-    I: InputParser,
-{
+impl<I> InputHandleSystem<I> {
     pub fn new() -> Self {
         InputHandleSystem {
             _marker: PhantomData,
-            input_buffer: CircularBuffer::new(I::BUFFER_SIZE),
         }
     }
 }
@@ -31,9 +24,29 @@ impl<'s, I> System<'s> for InputHandleSystem<I>
 where
     I: InputParser,
 {
-    type SystemData = (Read<'s, InputHandler<I::BindingTypes>>,);
+    type SystemData = (
+        Read<'s, InputHandler<I::BindingTypes>>,
+        Write<'s, EventChannel<I::Event>>,
+        Write<'s, I::InputSignal>,
+        WriteExpect<'s, CircularBuffer<I::InputSignal>>,
+        I::SystemData,
+    );
 
-    fn run(&mut self, (input,): Self::SystemData) {
-        self.input_buffer.push(I::add_buffer(&input));
+    fn run(
+        &mut self,
+        (input, mut events, mut now_signal, mut input_buffer, mut system): Self::SystemData,
+    ) {
+        let prev = input_buffer.queue().iter().last().map(|p| *p);
+        let current = I::add_buffer(&input, prev);
+        input_buffer.push(current);
+
+        events.iter_write(I::parse_input(&input_buffer, &mut system));
+
+        *now_signal = current;
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        world.insert(CircularBuffer::<I::InputSignal>::new(I::BUFFER_SIZE));
+        Self::SystemData::setup(world);
     }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,4 +1,5 @@
 use amethyst::{
+    ecs::SystemData,
     input::{BindingTypes, InputHandler},
     utils::circular_buffer::CircularBuffer,
 };
@@ -6,13 +7,20 @@ use amethyst::{
 pub trait InputParser {
     const BUFFER_SIZE: usize; // 入力を覚えるF数
     type BindingTypes: BindingTypes; // 入力キー
-    type InputSignal; // バッファに詰める入力
-    type Event; // バッファに詰めた入力から生成された実際に各エンティティにくばるイベント
+    type InputSignal: 'static + Sync + Send + Copy + Default; // バッファに詰める入力(ビットフラグなどでまとめる)
+    type Event: 'static + Sync + Send; // バッファに詰めた入力から生成された実際に各エンティティにくばるイベント
+    type SystemData: for<'s> SystemData<'s>;
 
     // 入力を確認してバッファに信号を生成する
-    fn add_buffer(handler: &InputHandler<Self::BindingTypes>) -> Self::InputSignal;
+    fn add_buffer(
+        handler: &InputHandler<Self::BindingTypes>,
+        prev_input: Option<Self::InputSignal>,
+    ) -> Self::InputSignal;
 
     // バッファの信号をパースして処理するためのイベントを生成する
     // 格ゲーのコマンド入力とかに使う
-    fn parse_input(buffer: &mut CircularBuffer<Self::InputSignal>) -> Option<Self::Event>;
+    fn parse_input(
+        buffer: &CircularBuffer<Self::InputSignal>,
+        system: &mut Self::SystemData,
+    ) -> Option<Self::Event>;
 }
